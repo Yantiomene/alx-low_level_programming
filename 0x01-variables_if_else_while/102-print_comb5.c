9627545a22e7463f43446ec68cf9c392192ec818
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-02-17 09:31:34 -0800

    Adding a C program to print all possible combinaitions of two two digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints all combinations of 2  2 digit number
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k, l, tmp1, tmp2;
+
+	i = 0;
+	while (i <= 9)
+	{
+		j = 0;
+		while (j <= 9)
+		{
+			k = 0;
+			while (k <= 9)
+			{
+				l = 0;
+				while (l <= 9)
+				{
+					tmp1 = i * 10 + j;
+					tmp2 = k * 10 + l;
+					if (tmp1 < tmp2)
+					{
+						putchar(i + '0');
+						putchar(j + '0');
+						putchar(' ');
+						putchar(k + '0');
+						putchar(l + '0');
+						if (i == 9 && j == 8 && k == 9 && l == 9)
+							break;
+						putchar(',');
+						putchar(' ');
+					}
+					l++;
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
