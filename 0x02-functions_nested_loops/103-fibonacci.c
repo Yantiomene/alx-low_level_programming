2f66c1e5a89191a9a92a507492a20cedd753bcfc
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-02-21 14:39:32 -0800

    Adding a program to print the sum of even term of fibonaaci less then 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints sum of even term of fibonacci less than 4,000,000
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int fb, fb1, fb2, sum;
+
+	fb1 = 1;
+	fb2 = 2;
+	sum = 2;
+	fb = fb1 + fb2;
+	while (fb <= 4000000)
+	{
+		fb = fb1 + fb2;
+		if (fb % 2 == 0)
+			sum += fb;
+		fb1 = fb2;
+		fb2 = fb;
+	}
+	printf("%ld \n", sum);
+	return (0);
+}
