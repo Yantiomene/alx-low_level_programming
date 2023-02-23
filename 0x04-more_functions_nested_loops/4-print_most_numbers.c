4cd238f7e4096bf6115575ea0ff507885c4fe71c
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-02-23 11:34:01 -0800

    Addinga function to prints number from 0 to 9 except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints numbers from 0 to 9 except 2 and 4
+ *
+ */
+void print_most_numbers(void)
+{
+	int c = 0;
+
+	while (c <= 9)
+	{
+		if (c != 2 && c != 4)
+			_putchar(c + '0');
+		c++;
+	}
+	_putchar('\n');
+}
