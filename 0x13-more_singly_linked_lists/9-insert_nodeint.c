0785c630ff06e41a2261884f2687d1fcfac35277
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-04-05 09:25:48 -0700

    [update] fixing some cases

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		for (i = 0; i < (idx - 1) && current != NULL; i++)
 			current = current->next;
-		if (current == NULL || (current->next == NULL && i == idx - 1))
+		if (current == NULL)
 			return (NULL);
 		new->next = current->next;
 		current->next = new;
