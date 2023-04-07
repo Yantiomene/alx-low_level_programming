00baeacaf7d72301143a35cd9e7de7d4c00eecb5
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-04-07 03:59:58 -0700

    [Fix] fixing a bug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,19 +10,19 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t num_write = 0, len;
+	int num_write = 0, len;
 
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == STDERR_FILENO)
+	if (fd == -1)
 		return (-1);
 	if (!text_content)
 		text_content = "";
 	for (len = 0; text_content[len]; len++)
 		;
 	num_write = write(fd, text_content, len);
-	if (num_write == STDERR_FILENO)
+	if (num_write == -1)
 		return (-1);
 	close(fd);
 	return (1);
