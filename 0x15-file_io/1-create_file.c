94edaa692dfd7cdf6b7ee2aea494f737929f3f2e
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-04-07 02:45:11 -0700

    Adding afunction to create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: path to the file
+ * @text_content: string to wrtie in the wrtie
+ *
+ * Return: 1(success), -1(failed)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t num_write = 0;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == STDERR_FILENO)
+		return (-1);
+	if (text_content)
+		num_write = write(fd, text_content, strlen(text_content));
+	if (num_write == STDERR_FILENO)
+		return (-1);
+	close(fd);
+	return (1);
+}
