e1837a8382f3f07478cd16210d1a68969a852d03
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-09-13 04:19:52 -0700

    [Feature] Adding an interpolation search algo

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,55 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - searches a value in a sorted array of int
+ * @array: pointer to the first element of array
+ * @size: number of element in array
+ * @value: value to search for
+ *
+ * Return: the index of the element or -1 if not found
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t low, high, pos;
+
+	if (!array || size == 0)
+		return (-1);
+	low = 0;
+	high = size - 1;
+
+	while (array[low] != array[high])
+	{
+		pos = low + (((double)(high - low) / (array[high] - array[low]))
+			     * (value - array[low]));
+		if (pos > high || pos < low)
+		{
+			printf("Value checked array[%lu] is out of range\n",
+			       pos);
+			return (-1);
+		}
+		if (array[pos] == value)
+		{
+			printf("Value checked array[%lu] = [%d]\n", pos,
+			       array[pos]);
+			return (pos);
+		}
+		if (array[pos] < value)
+		{
+			low = pos + 1;
+			printf("Value checked array[%lu] = [%d]\n", pos,
+			       array[pos]);
+		} else
+		{
+			high = pos - 1;
+			printf("Value checked array[%lu] = [%d]\n", pos,
+			       array[pos]);
+		}
+	}
+	if (value == array[low])
+	{
+		printf("Value checked array[%lu] = [%d]\n", pos,
+		       array[low]);
+		return (low);
+	}
+	return (-1);
+}
