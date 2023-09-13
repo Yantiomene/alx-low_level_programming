8774ee24a32e1c69159be245b79a00bdc545cd29
Yaninthe Tiomene <yan2016tiomene@gmail.com>
2023-09-13 04:32:25 -0700

    [Fix] Fixing betty style error

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -10,46 +10,39 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t low, high, pos;
+	size_t low = 0, high = size - 1, pos;
 
 	if (!array || size == 0)
 		return (-1);
-	low = 0;
-	high = size - 1;
-
 	while (array[low] != array[high])
 	{
 		pos = low + (((double)(high - low) / (array[high] - array[low]))
 			     * (value - array[low]));
 		if (pos > high || pos < low)
 		{
-			printf("Value checked array[%lu] is out of range\n",
-			       pos);
-			return (-1);
+		printf("Value checked array[%lu] is out of range\n", pos);
+		return (-1);
 		}
 		if (array[pos] == value)
 		{
-			printf("Value checked array[%lu] = [%d]\n", pos,
-			       array[pos]);
-			return (pos);
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		return (pos);
 		}
 		if (array[pos] < value)
 		{
-			low = pos + 1;
-			printf("Value checked array[%lu] = [%d]\n", pos,
-			       array[pos]);
-		} else
+		low = pos + 1;
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		}
+		else
 		{
-			high = pos - 1;
-			printf("Value checked array[%lu] = [%d]\n", pos,
-			       array[pos]);
+		high = pos - 1;
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 		}
 	}
 	if (value == array[low])
 	{
-		printf("Value checked array[%lu] = [%d]\n", pos,
-		       array[low]);
-		return (low);
+	printf("Value checked array[%lu] = [%d]\n", pos, array[low]);
+	return (low);
 	}
 	return (-1);
 }
